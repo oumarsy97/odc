d244fe49dfb469737aff81628b7b24bfda36cc22
oumarsy97 <syoumar505@gmail.com>
2024-04-18 19:20:53 +0000

    update

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
-include 'index.html';
+$datas = [
+    ["libelle"=>"the last of us","category"=>"action","year"=>"2005"],
+    ["libelle"=>"adventure time","category"=>"comedy","year"=>"2020"],
+    ["libelle"=>"one piece","category"=>"manga","year"=>"2013"],
+    ["libelle"=>"black list","category"=>"action","year"=>"2005"],
+    ["libelle"=>"good doctor","category"=>"action","year"=>"2001"],
+    ["libelle"=>"my hero academia","category"=>"manga","year"=>"2020"],
+    ["libelle"=>"naruto","category"=>"manga","year"=>"2005"],
+    ["libelle"=>"the manifest","category"=>"action","year"=>"2001"]
+];
 include 'lister.html';
